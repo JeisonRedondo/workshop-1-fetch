b1c8dc5b6bdeb0bc625c96116f6543bb7e77edac
JeisonPC <jeida16@yahoo.es>
2022-10-31 17:58:40 -0500

    agregando primera presentacion de la aplicación :Class 12

--- src/index.js
+++ src/index.js
@@ -3,4 +3,30 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+// Web API:
+// Conectarnos al servidor
+window.fetch(url)
+// procesar la respuesta y convertirla en JSON
+    .then(respuesta => respuesta.json())
+// JSON -> Data -> Renderizar info browser 
+    .then(responseJSON => {
+        // console.log(data);
+        const allItems = [];
+        responseJSON.data.forEach((element) => {
+            // Crear elemento:imagen
+            const image = document.createElement('img');
+            // Crear titulo
+            const title = document.createElement('h2');
+            // crear precio
+            const price = document.createElement('div');
+            
+            const container = document.createElement('div');
+            container.append(image, title, price);
+            
+            allItems.push(container)
+            
+        });
+        document.body.append(...allItems);
+    })
